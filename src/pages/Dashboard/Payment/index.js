25edc0531185a50be2f64df2dcf16c303d90ebfa
Audamon <joaotamagno@gmail.com>
2022-01-19 15:09:34 -0300

    Payment page title

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,12 @@
+import Typography from "@material-ui/core/Typography";
+import styled from "styled-components";
+
 export default function Payment() {
-  return "Pagamento: Em breve!";
+  return (
+    <StyledTypography variant="h4"> Ingresso e Pagamento</StyledTypography>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px !important;
+`;
