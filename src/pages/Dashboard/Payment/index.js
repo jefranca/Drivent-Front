8a76bac3ab90dd38d65732123b01cb6dbbd7fb6e
Glauco Villas Boas <glappsmobile@gmail.com>
2022-01-20 15:12:08 -0300

    1-added fullRegistration parameter to UserContext, 2-retrieving registration status when accessing payment area

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,28 @@
+import { useContext } from "react";
+import UserContext from "../../../contexts/UserContext";
+import Typography from "@material-ui/core/Typography";
+import styled from "styled-components";
+
 export default function Payment() {
-  return "Pagamento: Em breve!";
+  const { userData } = useContext(UserContext);
+
+  if (!userData.fullRegistration) {
+    
+  }
+
+  console.log(userData);
+  return (
+    <>
+      <StyledTypography variant="h4"> Ingresso e pagamento </StyledTypography>
+      {userData.fullRegistration ? (
+        <span> Tá registrado </span>
+      ) : (
+        <span> Sem registro </span>
+      )}
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
