34093c6b4eba869b2f79c9cd68319ac43601a5c8
jefranca <jean13franca@gmail.com>
2022-01-20 22:35:40 -0300

    feat/hotel-acess with mocked payment

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,25 @@
+import UnauthorizedMessage from "../../../components/Dashboard/shared/UnauthorizedMessage";
+import Typography from "@material-ui/core/Typography";
+import styled from "styled-components";
+
 export default function Hotel() {
-  return "Hotel: Em breve!";
+  const isPaid = false;
+
+  return (
+    <>
+      <StyledTypography variant="h4"> Ingresso e pagamento </StyledTypography>
+      {isPaid ? (
+        <>Em breve!</>
+      ) : (
+        <UnauthorizedMessage>
+          Você precisa completar o pagamento antes de prosseguir pra escolha
+          de ingresso
+        </UnauthorizedMessage>
+      )}
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px !important;
+`;
