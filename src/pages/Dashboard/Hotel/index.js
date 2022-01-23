b6fe81abf8c5740895ed20db6af95848f67f647e
SrSauloss <saulosilva@super.ufam.edu.br>
2022-01-23 13:35:44 -0400

    Added basic layout for choosing hotels

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -8,20 +8,154 @@ export default function Hotel() {
   const { userData } = useContext(UserContext);
 
   return (
-    <>
-      <StyledTypography variant="h4"> Ingresso e pagamento </StyledTypography>
-      {userData.isPaid ? (
+    <Container>
+      <StyledTypography variant="h4">
+        Escolha de hotel e quarto
+      </StyledTypography>
+      {/*userData.isPaid ? (
         <>Em breve!</>
       ) : (
         <UnauthorizedMessage>
           Você precisa completar o pagamento antes de prosseguir pra escolha de
           ingresso
         </UnauthorizedMessage>
-      )}
-    </>
+      )*/}
+
+      <h2>Primeiro, escolha seu hotel</h2>
+      <BoxHotels>
+        <HotelCard>
+          <img
+            src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/itemimages/38/86/38862_v9.jpeg"
+            alt="hotel"
+          />
+          <h3>Driven Resort</h3>
+          <Description>
+            <Title>Tipos de acomodação:</Title>
+            <span>Single e Double</span>
+          </Description>
+          <Description>
+            <Title>Vagas disponíveis:</Title>
+            <span>103</span>
+          </Description>
+        </HotelCard>
+        <HotelCard>
+          <img
+            src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/itemimages/38/86/38862_v9.jpeg"
+            alt="hotel"
+          />
+          <h3>Driven Resort</h3>
+          <Description>
+            <Title>Tipos de acomodação:</Title>
+            <span>Single e Double</span>
+          </Description>
+          <Description>
+            <Title>Vagas disponíveis:</Title>
+            <span>103</span>
+          </Description>
+        </HotelCard>
+        <HotelCard>
+          <img
+            src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/itemimages/38/86/38862_v9.jpeg"
+            alt="hotel"
+          />
+          <h3>Driven Resort</h3>
+          <Description>
+            <Title>Tipos de acomodação:</Title>
+            <span>Single e Double</span>
+          </Description>
+          <Description>
+            <Title>Vagas disponíveis:</Title>
+            <span>103</span>
+          </Description>
+        </HotelCard>
+        <HotelCard>
+          <img
+            src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/itemimages/38/86/38862_v9.jpeg"
+            alt="hotel"
+          />
+          <h3>Driven Resort</h3>
+          <Description>
+            <Title>Tipos de acomodação:</Title>
+            <span>Single e Double</span>
+          </Description>
+          <Description>
+            <Title>Vagas disponíveis:</Title>
+            <span>103</span>
+          </Description>
+        </HotelCard>
+        <HotelCard>
+          <img
+            src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/itemimages/38/86/38862_v9.jpeg"
+            alt="hotel"
+          />
+          <h3>Driven Resort</h3>
+          <Description>
+            <Title>Tipos de acomodação:</Title>
+            <span>Single e Double</span>
+          </Description>
+          <Description>
+            <Title>Vagas disponíveis:</Title>
+            <span>103</span>
+          </Description>
+        </HotelCard>
+      </BoxHotels>
+    </Container>
   );
 }
 
 const StyledTypography = styled(Typography)`
   margin-bottom: 20px !important;
 `;
+
+const Container = styled.div`
+  font-family: "Roboto";
+  & > h2 {
+    color: #8e8e8e;
+    font-size: 20px;
+    margin-bottom: 18px;
+  }
+`;
+
+const BoxHotels = styled.div`
+  height: 300px;
+  width: 100%;
+  display: flex;
+  overflow-x: auto;
+`;
+
+const HotelCard = styled.div`
+  background: #f1f1f1;
+  height: 265px;
+  width: 196px;
+  border-radius: 10px;
+  font-family: "Roboto";
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  margin-right: 20px;
+  padding: 18px 14px;
+  img {
+    height: 110px;
+    width: 170px;
+    border-radius: 5px;
+  }
+  & > h3 {
+    font-size: 20px;
+    font-weight: 400;
+    color: #343434;
+  }
+`;
+
+const Description = styled.div`
+  font-size: 12px;
+  display: flex;
+  flex-direction: column;
+  margin-top: 12px;
+  color: #3c3c3c;
+`;
+
+const Title = styled.span`
+  font-weight: 700;
+  margin-bottom: 2px;
+`;
+const RoomCard = styled.div``;
