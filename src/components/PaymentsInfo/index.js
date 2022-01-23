4e66aa52468d5ad2e7897127376c10adbcf4ac00
Audamon <joaotamagno@gmail.com>
2022-01-23 15:29:26 -0300

    No comented code

--- src/components/PaymentsInfo/index.js
+++ src/components/PaymentsInfo/index.js
@@ -95,6 +95,4 @@ export default function PaymentInfo() {
       )}
     </>
   );
-
-  // return <TicketSelection />;
 }
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,9 +1,5 @@
-//import { useContext } from "react";
-//import UserContext from "../../../contexts/UserContext";
 import PaymentInfo from "../../../components/PaymentsInfo/index.js";
 
 export default function Payment() {
-  // const { userData } = useContext(UserContext);
-
   return <PaymentInfo />;
 }
