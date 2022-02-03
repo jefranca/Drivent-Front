1f16f7363e8b5f0282e83b24b6a0500b9faef11f
SrSauloss <saulosilva@super.ufam.edu.br>
2022-02-02 23:18:42 -0400

    Added activity mobile layout

--- src/components/ActivitiesTable/Column.js
+++ src/components/ActivitiesTable/Column.js
@@ -23,11 +23,17 @@ const ColumContent = styled.div`
   border-right: 1px solid #d7d7d7;
   border-bottom: 1px solid #d7d7d7;
   border-left: ${({ index }) => (index === 0 ? "1px solid #D7D7D7" : "")};
+  @media (max-width: 600px) {
+    border-left: 1px solid #d7d7d7;
+  }
 `;
 
 const ColumnTitle = styled(Typography)`
   text-align: center;
   color: #838383;
+  @media (max-width: 600px) {
+    padding-top: 5px;
+  }
 `;
 
 export default Column;
