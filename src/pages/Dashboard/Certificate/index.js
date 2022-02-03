8f4c8371d7704be33ec6ad406c568fdc9a198547
Glauco Villas Boas <glappsmobile@gmail.com>
2022-02-03 03:28:22 -0300

    User can download their certificate

--- src/pages/Dashboard/Certificate/index.js
+++ src/pages/Dashboard/Certificate/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import UserContext from "../../../contexts/UserContext";
 import UnauthorizedMessage from "../../../components/shared/UnauthorizedMessage";
 import CertificateImage from "../../../components/CertificateImage";
@@ -7,6 +7,8 @@ import Button from "../../../components/Form/Button";
 import Title from "../../../components/shared/Title";
 import EventInfoContext from "../../../contexts/EventInfoContext";
 import useApi from "../../../hooks/useApi";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 export default function Certificate() {
   const { userData } = useContext(UserContext);
@@ -14,6 +16,29 @@ export default function Certificate() {
   const { enrollment, activity } = useApi();
   const [userName, setUserName] = useState("");
   const [activitiesData, setActivitiesData] = useState({});
+  const [isDownloading, setIsDownloading] = useState(false);
+  const certificateRef = useRef();
+
+  const handleDownloadPdf = () => {
+    setIsDownloading(true);
+  };
+
+  const download = async() => {
+    const element = certificateRef.current;
+    const canvas = await html2canvas(element);
+
+    const data = canvas.toDataURL("image/jpg");
+    const pdf = new jsPDF();
+    pdf.addImage(data, "JPEG", 0, 0);
+    pdf.save("certificado.pdf");
+  };
+
+  useEffect(() => {
+    if (isDownloading) {
+      download();
+      setIsDownloading(false);
+    }
+  }, [isDownloading]);
 
   useEffect(() => {
     enrollment.getPersonalInformations()
@@ -32,28 +57,37 @@ export default function Certificate() {
   return (
     <>
       <Title> Certificação </Title>
-      <CertificateImage
-        name={userName}
-        event={eventInfo.eventTitle}
-        year={activitiesData.year}
-        startDay={activitiesData.startDay}
-        startMonth={activitiesData.startMonth}
-        endDay={activitiesData.endDay}
-        endMonth={activitiesData.endMonth}
-        type={type}
-        hours={activitiesData.totalHours}
-        border={eventInfo.backgroundImage}
-        imgSrc={eventInfo.logoImage}
-      />
-
+      <CertificateContainer ref={certificateRef} isDownloading={isDownloading}>
+        <CertificateImage
+          name={userName}
+          event={eventInfo.eventTitle}
+          year={activitiesData.year}
+          startDay={activitiesData.startDay}
+          startMonth={activitiesData.startMonth}
+          endDay={activitiesData.endDay}
+          endMonth={activitiesData.endMonth}
+          type={type}
+          hours={activitiesData.totalHours}
+          border={eventInfo.backgroundImage}
+          imgSrc={eventInfo.logoImage}
+        />      
+      </CertificateContainer>
+    
       <SubmitContainer>
-        <Button>
+        <Button onClick={handleDownloadPdf}>
           Baixar
         </Button>
       </SubmitContainer>
     </>
   );
 }
+
+const CertificateContainer = styled.div`
+  ${({ isDownloading }) => isDownloading && `
+    position: absolute;
+    width: 721px;
+  `}
+`;
 
 const SubmitContainer = styled.div`
   margin-top: 20px !important;
